94cf074225150fec0526b4ee079b0607073a10b5
medwejonok <sliceofbear@bk.ru>
2023-12-28 00:36:09 +0300

    test

--- flask_ckeditor/static/full/plugins/savestatic/plugin.js
+++ flask_ckeditor/static/full/plugins/savestatic/plugin.js
@@ -7,17 +7,17 @@ CKEDITOR.plugins.add('savestatic', {
               var data_html = editor.getData();
       
               var data = {"name" : '1', "description" : '2', "context" : data_html}
-              console.log(JSON.stringify(data))
+              // console.log(JSON.stringify(data))
 
 
               fetch('pages/api', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json; charset=utf-8'
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(data)
               })
-              .then(response => response.json())
+              .then(response => console.log(response.json()))
               .then(data => console.log('Success:', data))
               .catch((error) => console.error('Error:', error));
           }
