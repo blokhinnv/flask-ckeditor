5dc5f4030253feb6da3e96a981b1c3bc590dd827
medwejonok <sliceofbear@bk.ru>
2023-12-28 00:31:15 +0300

    test

--- flask_ckeditor/static/full/plugins/savestatic/plugin.js
+++ flask_ckeditor/static/full/plugins/savestatic/plugin.js
@@ -5,13 +5,11 @@ CKEDITOR.plugins.add('savestatic', {
 
               // Код для сохранения данных на сервер
               var data_html = editor.getData();
-              var sanitized_data_html = data_html.replace(/[\u007F-\uFFFF]/g, function(chr) {
-                return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substr(-4);
-              });
-              
-              var data = {"name" : '1', "description" : '2', "context" : sanitized_data_html};
-              // var data = {"name" : '1', "description" : '2', "context" : data_html}
+      
+              var data = {"name" : '1', "description" : '2', "context" : data_html}
               console.log(JSON.stringify(data))
+
+
               fetch('pages/api', {
                 method: 'POST',
                 headers: {
@@ -28,7 +26,7 @@ CKEDITOR.plugins.add('savestatic', {
           label: 'Сохранить',
           command: 'savestatic',
           toolbar: 'save',
-          icon: null
+          icon: 'save'
       });
   }
 });
