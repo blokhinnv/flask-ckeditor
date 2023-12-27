f8bad55dda649b817cc5e60d7b6f2b93cd409582
medwejonok <sliceofbear@bk.ru>
2023-12-28 02:13:25 +0300

    test

--- flask_ckeditor/static/full/plugins/inputsdb/dialogs/inputsdb.js
+++ flask_ckeditor/static/full/plugins/inputsdb/dialogs/inputsdb.js
@@ -1,5 +1,5 @@
 CKEDITOR.dialog.add('inputsdb', function(editor) {
-  var dataPath = editor.config.inputsdbDataPath || 'api/charts/list/'; 
+  var dataPath = editor.config.inputsdbDataPath || 'api/list'; 
 
   function fetchData() {
     return new Promise(function(resolve, reject) {
@@ -47,6 +47,7 @@ CKEDITOR.dialog.add('inputsdb', function(editor) {
               var element = this.getDialog().getContentElement('tab1', 'select')
               fetchData().then(function(data) {
                 images = data;
+                console.log(images)
                 fillSelect(element, images);
               }).catch(function(error) {
                 console.error(error);
